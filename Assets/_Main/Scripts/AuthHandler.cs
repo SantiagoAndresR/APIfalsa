baf403164a2ad91cf07524d99d67818d11bae906
SantiagoAndresR <109627166+SantiagoAndresR@users.noreply.github.com>
2023-08-22 11:07:09 -0500

    yes

--- Assets/_Main/Scripts/AuthHandler.cs
+++ Assets/_Main/Scripts/AuthHandler.cs
@@ -3,18 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using TMPro;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
 
 public class AuthHandler : MonoBehaviour
 {
-    public TMP_InputField UsernameInput;
-    public TMP_InputField PasswordInput;
+    public string apiUrl = "https://sid-restapi.onrender.com/api/";
 
-    public string apiUrl = "https://sid-restapi.onrender.com/api";
+    TMP_InputField UsernameInput;
+    TMP_InputField PasswordInput;
+
+    private string Token;
+    private string Username;
+
 
     private void Start()
     {
-        UsernameInput = GameObject.Find("UsernameInput").GettComponent<TMP_ImputField>();
-        PasswordInput = GameObject.Find("PasswordInput").GettComponent<TMP_ImputField>();
+        Token = PlayerPrefs.GetString("token");
+
+        if (string.IsNullOrEmpty(Token))
+        {
+            Debug.Log("No hay Token almacenado");
+        }
+        else
+        {
+            Username = PlayerPrefs.GetString("username");
+            StartCoroutine(GetPerfil(Username));
+        }
+
+
+        UsernameInput = GameObject.Find("UsernameInput").GetComponent<TMP_InputField>(); 
+        PasswordInput = GameObject.Find("PasswordInput").GetComponent<TMP_InputField>();  
+
     }
 
     public void Register()
@@ -28,61 +49,6 @@ public class AuthHandler : MonoBehaviour
         StartCoroutine(SendRegister(json));
     }
 
-    IEnumerator SendRegister(string json)
-    {
-        UnityWebRequest request = UnityWebRequest.Put(apiUrl + "/usuarios", json);
-        request.SetRequestHeader("Content-Type", "application/json");
-        request.method = "POST";
-        yield return request.SendWebRequest();
-
-        if (request.isNetworkError)
-        {
-            Debug.log("Network Error :" + request.error);
-        }
-        else
-        {
-            Debug.log(request.downloadHandler.text);
-            if (request.responseCode == 200)
-            {
-                AuthData data = JsonUtility.FromJson<AuthData>(request.downloadHandler.text);
-
-                Debug.log("Se registro el usuario con id: " + data.usuario._id);
-            }
-            else
-            {
-                Debug.log(request.error);
-            }
-        }
-    }
-
-    IEnumerator SendLogin(string json)
-    {
-        UnityWebRequest request = UnityWebRequest.Put(apiUrl + "auth/login", json);
-        request.SetRequestHeader("Content-Type", "application/json");
-        request.method = "POST";
-        yield return request.SendWebRequest();
-
-        if (request.isNetworkError)
-        {
-            Debug.log("Network Error :" + request.error);
-        }
-        else
-        {
-            Debug.log(request.downloadHandler.text);
-            if (request.responseCode == 200)
-            {
-                AuthData data = JsonUtility.FromJson<AuthData>(request.downloadHandler.text);
-
-                Debug.log("Se inicio sesion del usuario: " + data.usuario.username);
-                Debug.log(data.token);
-            }
-            else
-            {
-                Debug.log(request.error);
-            }
-        }
-    }
-
     public void Login()
     {
         AuthData authData = new AuthData();
@@ -94,6 +60,92 @@ public class AuthHandler : MonoBehaviour
         StartCoroutine(SendLogin(json));
     }
 
+    IEnumerator GetPerfil(string username)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(apiUrl + "usuarios/" + username);
+        request.SetRequestHeader("x-token", Token);
+        yield return request.SendWebRequest();
+        if (request.isNetworkError)
+        {
+            Debug.Log("Network Error :" + request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+            if (request.responseCode == 200)
+            {
+                AuthData data = JsonUtility.FromJson<AuthData>(request.downloadHandler.text);
+                Debug.Log("Se del usuario: " + data.usuario.username);
+                Debug.Log("Su Score es: " + data.usuario.data.score);
+                SceneManager.LoadScene("Game");
+            }
+            else
+            {
+                Debug.Log(request.error);
+            }
+        }
+    }
+
+    IEnumerator SendRegister(string json)
+    {
+        UnityWebRequest request = UnityWebRequest.Put(apiUrl + "/usuarios", json);
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.method = "POST";
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError)
+        {
+            Debug.Log("Network Error :" + request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+            if (request.responseCode == 200)
+            {
+                AuthData data = JsonUtility.FromJson<AuthData>(request.downloadHandler.text);
+
+                Debug.Log("Se registro el usuario con id: " + data.usuario._id);
+            }
+            else
+            {
+                Debug.Log(request.error);
+            }
+        }
+    }
+
+
+    IEnumerator SendLogin(string json)
+    {
+        UnityWebRequest request = UnityWebRequest.Put(apiUrl + "auth/login", json);
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.method = "POST";
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError)
+        {
+            Debug.Log("Network Error :" + request.error);
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+            if (request.responseCode == 200)
+            {
+                AuthData data = JsonUtility.FromJson<AuthData>(request.downloadHandler.text);
+
+                Debug.Log("Se inicio sesion del usuario: " + data.usuario.username);
+
+                PlayerPrefs.SetString("token", data.token);
+                PlayerPrefs.SetString("username", data.usuario.username);
+                SceneManager.LoadScene("Game");
+            }
+            else
+            {
+                Debug.Log(request.error);
+            }
+        }
+    }
+
+
 
 }
 
@@ -103,15 +155,25 @@ public class AuthData
 {
     public string username;
     public string password;
-    public UserData usuario;
+    public DataUser usuario;
     public string token;
+    public UserT[] usuarios;
 }
 
+
 [System.Serializable]
-public class UserData
+public class DataUser
 {
     public string _id;
     public string username;
     public bool estado;
+    public UserT data;
 }
 
+[System.Serializable]
+public class UserT
+{
+    public int score;
+    public string data;
+    public DataUser[] Friends;
+}
